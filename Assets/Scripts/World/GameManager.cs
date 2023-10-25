8655539d4603b4bc07e9454484ccf05d850a63c5
DESKTOP-CBTLII3\Bryan <hc-bryan-@hotmail.com>
2023-10-25 17:06:49 -0600

    Ammo & health UI

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    //public GameObject canvasObject;
+    public TextMeshProUGUI ammoText;
+    public TextMeshProUGUI healthText;
+    public int health = 100;
+
     public static GameManager Instance {  get; private set; }
     public int gunAmmo = 10;
 
@@ -12,4 +18,16 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
     }
+
+    private void Start()
+    {
+        //ammoText = canvasObject.transform.Find("AmmoScript").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        ammoText.text = gunAmmo.ToString();
+        healthText.text = health.ToString();
+    }
+
 }
